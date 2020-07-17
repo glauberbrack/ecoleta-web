d2252cb6941ba2b12fd12e983748d9da2c2638b4
Glauber Brack <glauber@brack.com.br>
2020-07-17 18:02:23 -0300

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
   return (
-        <p>
-         Oieee
-        </p>
+    <div>
+      <Header/>
+      <p>oies</p>
+    </div>
   );
 }
 
