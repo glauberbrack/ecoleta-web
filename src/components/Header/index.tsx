d5e6a6b8515aa549c749ab8962bd62adb0f81a16
Glauber Brack <glauber@brack.com.br>
2020-07-27 23:52:38 -0300

    map integration

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { title } from 'process';
 
 interface HeaderProps {
     title: string;
--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import './styles.css'
 
@@ -60,6 +61,14 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-20.9385845, -48.4893146]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-20.9385845, -48.4893146]} />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
